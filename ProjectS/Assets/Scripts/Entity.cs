7ad841e721c558c21aa026a5e9142d451ac818cc
seocheonf <seocheonf@gmail.com>
2023-08-12 15:08:31 +0900

    Entity Structure class
    Entity Structure class
    Structure inherited by Entity

---
+++ ProjectS/Assets/Scripts/Entity.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    /*
+     * 
+     * hp : 엔티티의 체력
+     * damage : 엔티티의 데미지
+     * attacked : 피격 가능 여부
+     * 
+     */
+    public int hp;
+    public int damage;
+    public bool attacked;
+}
