26a90476ba372d23b2be054d90d8c573c60623f8
seocheonf <seocheonf@gmail.com>
2023-08-12 15:42:14 +0900

    Test2
    Test2

--- ProjectS/Assets/Scripts/Leader/Enemy.cs
+++ ProjectS/Assets/Scripts/Leader/Enemy.cs
@@ -4,16 +4,5 @@ using UnityEngine;
 
 public class Enemy : Entity
 {
-
-    private void Start()
-    {
-        StartCoroutine(Test());
-    }
-
-    IEnumerator Test()
-    {
-        yield return new WaitForSeconds(1.0f);
-        Debug.Log("test");
-    }
 }
 
