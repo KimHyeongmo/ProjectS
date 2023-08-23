07a1fbca32bdd7c6aadd84e42b16046eda45d36a
seocheonf <seocheonf@gmail.com>
2023-08-24 00:27:26 +0900

    revert
    revert

--- ProjectS/Assets/Scripts/Leader/Enemy.cs
+++ ProjectS/Assets/Scripts/Leader/Enemy.cs
@@ -4,31 +4,5 @@ using UnityEngine;
 
 public class Enemy : Entity
 {
-    int previous_hp;
-
-    public int speed;
-
-    private void Start()
-    {
-        previous_hp = this.hp;
-    }
-
-    private void Update()
-    {
-
-
-        if(previous_hp > this.hp)
-        {
-            Debug.Log("Ouch!!");
-        }
-
-        if(this.hp<=0)
-        {
-            Debug.Log("Dead");
-        }
-
-        previous_hp = this.hp;
-
-    }
 }
 
